4edcb1a0b27a85d9bca9a9649037a7edf6941bd1
mohamed-aminmalki <mohamed-amin.malki@student.ehb.be>
2021-10-07 11:29:08 +0200

    toon pokemons img

--- session2/script.js
+++ session2/script.js
@@ -1,14 +1,30 @@
 console.log("linked");
 window.onload = () =>{
-function fetchPokemonApi(){
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-    .then(response => response.json())
-    .then(function(data){
 
-        
-    data.results.forEach(function(pokemon){
-        fetchPokemonData(pokemon); 
-      })
-     })
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+
+    .then(response => response.json())
+    .then(data => getPokemonsData(data.results))
   }
+
+  function getPokemonsData(pokemons){
+      console.log(pokemons);
+
+
+  pokemons.forEach(pokemon => 
+
+    fetch(pokemon.url)
+    .then(response => response.json())
+    .then(data => printHtml(data))
+    )
+
+  }
+
+function printHtml(pokemonData){
+
+  console.log(pokemonData);
+
+  document.getElementById('container').innerHTML += `
+  <div><img class="card-img-top" src="${pokemonData.sprites.front_default}" alt="img"></div>`
+
 }
