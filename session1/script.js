764d9a1d38905afa98664492dce28321ec29fa3d
mohamed-aminmalki <mohamed-amin.malki@student.ehb.be>
2021-10-13 23:52:14 +0200

    return text

--- session1/script.js
+++ session1/script.js
@@ -3,7 +3,15 @@ window.onload = () =>{
 
     document.getElementById('form')
     .addEventListener('submit', event => {
-        console.log("exercise 2 listen to events")
+        event.preventDefault();
+
+        let name =  document.getElementById('nameInput').value
+        let email =  document.getElementById('emailInput').value
+        let order =  document.getElementById('orderInput').value
+
+        
+        return `<p>The order for the customer ${name} is the following:  
+        ${order}. The customer may be notified by email: ${email}</p>`;
     });
 
 }
