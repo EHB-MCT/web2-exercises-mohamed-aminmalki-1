8b7b8d6396b7b8c1e73beeec37faa8dff1254f81
mohamed-aminmalki <mohamed-amin.malki@student.ehb.be>
2021-10-13 23:44:02 +0200

    Exercise 2 - Listen to events

--- session1/script.js
+++ session1/script.js
@@ -1,3 +1,9 @@
 window.onload = () =>{
     console.log("linked")
+
+    document.getElementById('form')
+    .addEventListener('submit', event => {
+        console.log("exercise 2 listen to events")
+    });
+
 }
