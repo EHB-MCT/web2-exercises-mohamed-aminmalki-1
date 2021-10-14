62d5aaaa4fae8bf48b4ae606110b30c70ce2f5fb
mohamed-aminmalki <mohamed-amin.malki@student.ehb.be>
2021-10-14 11:57:40 +0200

    show data in console

--- session3/3b/script.js
+++ session3/3b/script.js
@@ -1,3 +1,11 @@
+let baseurl = 'http://www.omdbapi.com/?i=tt3896198&apikey=';
+let apikey = '647ae3a3'
+
 window.onload = () =>{
     console.log(`linked`)
+
+fetch(baseurl + apikey)
+    .then(response => response.json())
+    .then(data => console.log(data))
+
 }
