caeea0c02efecf3d6e882a9d1d8cc881f7a5a901
mohamed-aminmalki <mohamed-amin.malki@student.ehb.be>
2021-10-14 10:16:33 +0200

    show messages on screen

--- session3/script.js
+++ session3/script.js
@@ -6,12 +6,14 @@ window.onload = () =>{
     .addEventListener('submit', () => {
     
         let inputNumber = document.getElementById('inputNumber').value;
-        console.log(resultNumber);
+        console.log(inputNumber);
 
-        if (randomNumber == inputNumber){
+        if (randomNumber < inputNumber){
+            console.log(`The mystery number is lower. Guess again!`)
+        }else if(randomNumber > inputNumber){
+            console.log(`The mystery number is higher. Guess again!`)
+        }else if(randomNumber == inputNumber){
             console.log(`You have guessed the mystery number!`)
-        }else {
-            console.log(`nice try`)
         }
     });
 }
